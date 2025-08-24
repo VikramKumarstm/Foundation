3eef58d464552efd40c2ba667efafc6ceaf52f04
VikramKumarstm <vikramclg0000@gmail.com>
2025-08-24 08:32:25 +0530

    Resolve after some time

--- Loop/LCM.java
+++ Loop/LCM.java
@@ -12,12 +12,12 @@ public class LCM {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        int max = (num1 > num2) ? num1 : num2;
+        int min = (num1 > num2) ? num2 : num1;
         int lcm = 1;
 
-        for(int i=1; i<=max; i++) {
+        for(int i=1; i<=min; i++) {
             
-            if(num1 % i == 0 && num2 % i == 0) {
+            if(num1 % i == 0 || num2 % i == 0) {
                 lcm *= i;
             }
         }
