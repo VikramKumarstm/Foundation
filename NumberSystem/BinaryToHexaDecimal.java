db9b37fdead4be7c2a2747e78b5529375f89085c
VikramKumarstm <vikramclg0000@gmail.com>
2025-08-31 23:07:10 +0530

    some problem

---
+++ NumberSystem/BinaryToHexaDecimal.java
@@ -0,0 +1,19 @@
+// Write a C program to convert Binary to Hexadecimal number system.
+
+package NumberSystem;
+
+import java.util.Scanner;
+
+public class BinaryToHexaDecimal {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter binary number :");
+        long binary = sc.nextLong();
+
+        
+
+
+    }
+}
